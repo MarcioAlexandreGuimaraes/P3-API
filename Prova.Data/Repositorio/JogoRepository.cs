10c3047d0508d4c59bde691c2afb49ec1ea2f694
MarcioAlexandreGuimaraes <marcio.guimaraes2615@gmail.com>
2025-12-17 20:40:33 -0300

    Add files via upload

---
+++ Prova.Data/Repositorio/JogoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prova2Bim.Dominio.Entidades;
+
+namespace Prova2Bim.Data.Repositorio
+{
+		public class JogoRepository
+		{
+			public void Inserir(Jogo jogo)
+			{
+			
+			}
+
+			public List<Jogo> ListarTodos()
+			{
+				return new List<Jogo>();
+			}
+		}
+
+	}
+
